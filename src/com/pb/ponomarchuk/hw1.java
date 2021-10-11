13790491ffd3cabd308deb3949b83a9718c53650
Alona Ponomarchuk <Alona.ponomarchuk@privatbank.ua>
2021-10-12 00:06:08 +0300

    init commit

---
+++ src/com/pb/ponomarchuk/hw1.java
@@ -0,0 +1,9 @@
+package com.pb.ponomarchuk;
+
+public class hw1 {
+    public static class HelloWorld {
+        public static void main(String[] args)  {
+            System.out.println("Hello World");
+        }
+    }
+}
